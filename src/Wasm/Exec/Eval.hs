82254f1ed53d12f042a373fd9b9961a577a64fa3
Gabor Greif <gabor@dfinity.org>
2021-12-19 01:29:36 +0100

    simplify

--- src/Wasm/Exec/Eval.hs
+++ src/Wasm/Exec/Eval.hs
@@ -521,8 +521,8 @@ step(Code cs cfg vs (e:es)) = (`runReaderT` cfg) $ do
           let [addr_dst, addr_src, count] = fromIntegral . i64_extend_u_i32 . fromIntegral <$> [dst, src, cnt]
           let range = if dst < src then [0 .. pred cnt] else tail $ enumFromThenTo cnt (pred cnt) 0
           eres <- lift $ lift $ runExceptT $
-            mapM_ (\addr -> Memory.loadPacked Pack8 ZX mem addr 0 I32Type) [addr_dst + pred count, addr_src + pred count]
-            mapM_ (\off -> Memory.loadPacked Pack8 ZX mem addr_src off I32Type >>= Memory.storePacked Pack8 mem addr_dst off) range
+            mapM_ (\addr -> Memory.loadPacked Pack8 ZX mem (addr + pred count) 0 I32Type) [addr_dst, addr_src]
+            >> mapM_ (\off -> Memory.loadPacked Pack8 ZX mem addr_src off I32Type >>= Memory.storePacked Pack8 mem addr_dst off) range
           case eres of
             Right () -> k vs' es
             Left exn -> k vs' (Trapping (memoryErrorString exn) @@ at : es)
